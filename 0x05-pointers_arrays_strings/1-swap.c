4ed72e814a87c86ded0ec3d6f2ea92ead857ca35
Fortune Johnson <fortunejohnson16@gmail.com>
2022-07-13 12:06:27 -0700

    mmm

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+
+
+/**
+ *  * swap_int - swap variable values
+ *
+ *   * @a: 1st Pointer
+ *
+ *    * @b: 2nd pointer
+ *
+ *     * Return: void
+ *
+ **/
+void swap_int(int *a, int *b)
+
+{
+
+		*a += *b;
+
+			*b = *a - *b;
+
+				*a = *a - *b;
+
+}
